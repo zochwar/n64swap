ef4a8a2df22c941d61300104d4496cfdd3d175a5
zochwar <zochwar@protonmail.com>
2023-08-16 21:07:42 +0200

    First version of my N64 byte-swapper

---
+++ README.md
@@ -0,0 +1,30 @@
+# n64swap
+
+A slightly over-engineered Nintendo 64 Byte Swapper
+
+## Usage
+n64swap <filename> [outputfile]
+
+The simplest usage is `n64swap file.v64`, this will convert your file to a Big-Endian (.z64) rom
+You can optionally add the output filename as the second argument
+
+There are also some option flags available
+* -r, --romtype <ROMTYPE>
+    * big-endian (commonly .z64)
+    * byte-swap  (commonly .v64)
+    * little-endian (commonly .n64)
+* -i, --identify
+    * Identify rom (and exit)
+* -f, --force
+    * Force overwrite output file
+* -h, --help
+    * Print help (see a summary with '-h')
+* -V, --version
+    * Print version
+
+## Dependencies
+This program is written in [Rust](https://www.rust-lang.org/)
+[Clap](https://github.com/clap-rs/clap) is used to parse the commandline, cargo will add this automatically
+
+## Building
+There's nothing fancy going on, `cargo build` should work
---
+++ src/main.rs
@@ -0,0 +1,210 @@
+use core::fmt;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::process::exit;
+
+use clap::{Parser, ValueEnum};
+
+// N64 header magic bytes
+const BIG_ENDIAN: [u8; 4] = [0x80, 0x37, 0x12, 0x40];
+const BYTE_SWAP: [u8; 4] = [0x37, 0x80, 0x40, 0x12];
+const LITTLE_ENDIAN: [u8; 4] = [0x40, 0x12, 0x37, 0x80];
+
+#[derive(Debug, PartialEq, Copy, Clone, ValueEnum)]
+enum RomType {
+    /// (commonly .z64)
+    BigEndian,
+    /// (commonly .v64)
+    ByteSwap,
+    /// (commonly .n64)
+    LittleEndian,
+}
+
+impl fmt::Display for RomType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RomType::BigEndian => write!(f, "BigEndian (.z64)"),
+            RomType::ByteSwap => write!(f, "ByteSwap (.v64)"),
+            RomType::LittleEndian => write!(f, "LittleEndian (.n64)"),
+        }
+    }
+}
+
+impl RomType {
+    fn get_file_ext(&self) -> &str {
+        match self {
+            &RomType::BigEndian => ".z64",
+            &RomType::ByteSwap => ".v64",
+            &RomType::LittleEndian => ".n64",
+        }
+    }
+
+    fn get_header_bytes(&self) -> &[u8; 4] {
+        match self {
+            &RomType::BigEndian => &BIG_ENDIAN,
+            &RomType::ByteSwap => &BYTE_SWAP,
+            &RomType::LittleEndian => &LITTLE_ENDIAN,
+        }
+    }
+}
+
+fn guess_type(ext: &str) -> Option<RomType> {
+    match ext.to_lowercase().as_str() {
+        ".z64" => Some(RomType::BigEndian),
+        ".v64" => Some(RomType::ByteSwap),
+        ".n64" => Some(RomType::LittleEndian),
+        _ => None,
+    }
+}
+
+fn identify_header(bytes: &[u8; 4]) -> Option<RomType> {
+    match bytes {
+        &BIG_ENDIAN => Some(RomType::BigEndian),
+        &BYTE_SWAP => Some(RomType::ByteSwap),
+        &LITTLE_ENDIAN => Some(RomType::LittleEndian),
+        _ => None,
+    }
+}
+
+fn detect_ext(filename: &String) -> Option<&str> {
+    if let Some(idx) = filename.rfind(".") {
+        filename.get(idx..)
+    } else {
+        None
+    }
+}
+
+fn swapper(bytes: &mut [u8; 4], src_type: RomType, dst_type: RomType) {
+    match (src_type, dst_type) {
+        (RomType::BigEndian, RomType::ByteSwap) | (RomType::ByteSwap, RomType::BigEndian) => {
+            bytes.swap(0, 1);
+            bytes.swap(2, 3);
+        }
+        (RomType::BigEndian, RomType::LittleEndian) | (RomType::LittleEndian, RomType::BigEndian) => {
+            bytes.swap(0, 3);
+            bytes.swap(1, 2);
+        }
+        (RomType::ByteSwap, RomType::LittleEndian) | (RomType::LittleEndian, RomType::ByteSwap) => {
+            bytes.swap(0, 2);
+            bytes.swap(1, 3);
+        }
+        _ => {}
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input Filename
+    filename: String,
+
+    /// Output filename
+    destination_filename: Option<String>,
+
+    /// Output type
+    #[arg(short, long)]
+    romtype: Option<RomType>,
+
+    /// Identify rom type (and exit)
+    #[arg(short, long, default_value_t = false)]
+    identify: bool,
+
+    /// Force overwrite output file
+    #[arg(short, long, default_value_t = false)]
+    force: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // Input file
+    let Ok(file) = File::open(&args.filename) else {
+        println!("Unable to open file: {}", &args.filename);
+        exit(1)
+    };
+    let mut buf = BufReader::new(file);
+    let mut bytes = [0; 4];
+
+    // Let's read the header
+    let Ok(_) = buf.read_exact(&mut bytes) else {
+        println!("Error reading file: {}", &args.filename);
+        exit(1);
+    };
+
+    let Some(filetype) = identify_header(&bytes) else {
+        println!("File {} not recognized!", &args.filename);
+        exit(1);
+    };
+
+    if args.identify {
+        println!("File {} is {}", &args.filename, filetype);
+        exit(0);
+    }
+
+    // Output file
+    let outfiletype = args.romtype.unwrap_or_else(|| { // If defined on the commandline, use that
+        args.destination_filename.as_ref() // Otherwise borrow the destination filename
+            .map(detect_ext).flatten() // Detect the extension
+            .map(guess_type).flatten() // Identify the type based on extension
+            .unwrap_or(RomType::BigEndian) // Or default to BigEndian
+    });
+
+    if filetype == outfiletype {
+        println!("File is already {}!", outfiletype);
+        exit(0);
+    }
+
+    let outfilename = args.destination_filename.unwrap_or_else(|| {
+        // If defined on the commandline, use that
+        let mut name = args.filename.clone(); // Otherwise, copy the input filename
+        let len = name.len(); // Get the filename length
+        if name.chars().nth(len - 4) == Some('.') { // Check if there's a 3-letter extension
+            name.truncate(len - 4); // Lop off the extension
+        }
+        name.push_str(outfiletype.get_file_ext()); // Add the standard extension for the output type
+        name
+    });
+
+    if args.filename == outfilename {
+        println!(
+            "Input and Output filenames are identical {}, consider renaming input file",
+            &outfilename
+        );
+        exit(1);
+    }
+
+    //    let Ok(outfile) = File::options()
+    let outfile = match File::options()
+        .write(true)
+        .create_new(!args.force)
+        .open(&outfilename)
+    {
+        Ok(file) => file,
+        Err(error) => {
+            println!(
+                "Unable to open file {} for output. Error {}",
+                &outfilename, error
+            );
+            exit(1);
+        }
+    };
+    let mut outbuf = BufWriter::new(outfile);
+    let Ok(_) = outbuf.write_all(outfiletype.get_header_bytes() ) else {
+        println!("Unable to write to output file!");
+        exit(1);
+    };
+
+    loop {
+        match buf.read_exact(&mut bytes) {
+            Ok(_) => {
+                swapper(&mut bytes, filetype, outfiletype);
+
+                let Ok(_) = outbuf.write_all(&bytes) else {
+                    println!("Error during output!");
+                    exit(1);
+                };
+            }
+            _ => break,
+        }
+    }
+}
